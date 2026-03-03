00b59596a31d56ca8045ec0350bb8e76052d3a1f
Bruno <bruno@tulyp.io>
2026-03-03 12:42:59 +0100

    chore: add better auth

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,11 @@
 import { config } from "@/config"
 import { drizzle } from "drizzle-orm/node-postgres"
+import * as auth from "@/db/schema/auth"
 
-export const db = drizzle(config.database.connectionString)
+export const db = drizzle({
+  connection: config.database.connectionString,
+  casing: "snake_case",
+  schema: {
+    ...auth,
+  },
+})
